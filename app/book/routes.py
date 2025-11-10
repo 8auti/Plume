870a83fb84f538e2821c09b4a6a9e5d98ff437d2
8auti <bautistasilvalosilla@gmail.com>
2025-11-10 18:46:22 -0300

    Modified: Card Styles

--- app/book/routes.py
+++ app/book/routes.py
@@ -9,12 +9,19 @@ book = Blueprint("book", __name__)
 def view(id):
     
     book = Book.query.get(id)
-    books = Book.query.limit(9).all()
+
+    # Libros relacionados (chamuyo)
+    related_books = Book.query.limit(9).all()
+
+    # Obtener otros libros del mismo autor (excluyendo el actual)
+    #same_author_books = [b for b in book.author.books if b.id != book.id][:8]
+    same_author_books = [b for b in book.author.books][:8]
+
 
     if book is None:
         flash("Libro no encontrado.", "danger")
         return redirect(url_for('main.home'))
     
-    return render_template("book/view.html", book = book, books = books)
+    return render_template("book/view.html", book = book, related_books = related_books, same_author_books = same_author_books)
 
 # <----- ABM ----->
